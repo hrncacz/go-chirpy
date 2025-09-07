e67c9e33c606e1ebce68076d50c2bf556c5c345d
hrncacz <hrncacz@gmail.com>
2025-09-07 16:35:17 +0200

    index.html

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hola")
+	mux := http.NewServeMux()
+	httpServer := &http.Server{}
+
+	httpServer.Addr = ":8080"
+	httpServer.Handler = mux
+
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+
+	defer httpServer.Close()
+
+	err := httpServer.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
 }
